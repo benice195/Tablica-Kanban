dbe184b7695eb394e41bc9b7dbcd5d2d494ab8a9
Wojciech Szczyrba <benice195@gmail.com>
2017-01-15 22:22:52 +0100

    wyswietlanie kart po zaladowaniu:

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ function setupColumns(columns) {
     columns.forEach(function (column) {
   		var col = new Column(column.id, column.name);
         board.createColumn(col);
+        setupCards(col, column.cards);
     });
 };
 function setupCards(col, cards) {
