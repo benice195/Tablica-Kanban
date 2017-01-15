5669b0aa6b4d471cfc158fb57683b18b50b32fa8
Wojciech Szczyrba <benice195@gmail.com>
2017-01-15 20:07:52 +0100

    spaces

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ function setupCards(col, cards) {
 	cards.forEach(function (card) {
         var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
     	col.createCard(card);
-  	});
+  	})
 };
 
 
